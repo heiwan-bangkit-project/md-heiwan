29ef1ec30611c2d4522678424f2410c735af2ef9
RadRasyad <rafidanyr21@gmail.com>
2023-05-22 09:10:57 +0700

    add require icons(24px), all fragment for MainActivity(added NavDrawer & Bottom Navigation)

---
+++ app/src/main/java/com/c23ps291/heiwan/ui/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.c23ps291.heiwan.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.c23ps291.heiwan.R
+import com.c23ps291.heiwan.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val drawerView: NavigationView = binding.navDrawerView
+        val navView: BottomNavigationView = binding.appBarMain.botNavView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_bookmark, R.id.navigation_profile, R.id.navigation_community, R.id.navigation_setting
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        drawerView.setNavigationItemSelectedListener { menuItem ->
+            drawerView.setupWithNavController(navController)
+
+            menuItem.isChecked = true
+            drawerLayout.close()
+            true
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
