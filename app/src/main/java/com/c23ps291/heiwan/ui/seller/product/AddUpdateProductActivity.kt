0a2098055b802e770e30d69817af365be816a3e7
RadRasyad <rafidanyr21@gmail.com>
2023-05-24 14:18:10 +0700

    Product

---
+++ app/src/main/java/com/c23ps291/heiwan/ui/seller/product/AddUpdateProductActivity.kt
@@ -0,0 +1,12 @@
+package com.c23ps291.heiwan.ui.seller.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c23ps291.heiwan.R
+
+class AddUpdateProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_product)
+    }
+}
---
+++ app/src/main/java/com/c23ps291/heiwan/ui/seller/product/ListProductActivity.kt
@@ -0,0 +1,12 @@
+package com.c23ps291.heiwan.ui.seller.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c23ps291.heiwan.R
+
+class ListProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_product)
+    }
+}
