4ff775bc8f2decc640cc1ca3812dc6ae51ff4d08
RadRasyad <rafidanyr21@gmail.com>
2023-05-25 13:09:03 +0700

    take photo from camera & gallery done

--- app/src/main/java/com/c23ps291/heiwan/ui/common/ImageChooseBottomSheetFragment.kt
+++ app/src/main/java/com/c23ps291/heiwan/ui/common/ImageChooseBottomSheetFragment.kt
@@ -1,26 +1,142 @@
 package com.c23ps291.heiwan.ui.common
 
+import android.Manifest
+import android.R.attr.bitmap
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.c23ps291.heiwan.R
+import com.c23ps291.heiwan.databinding.FragmentImageChooseBottomSheetBinding
+import com.c23ps291.heiwan.ui.search.SearchByImageActivity
+import com.c23ps291.heiwan.utils.createCustomTempFile
+import com.c23ps291.heiwan.utils.uriToFile
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.io.File
 
 
 class ImageChooseBottomSheetFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentImageChooseBottomSheetBinding? = null
+    private val binding get() = _binding
+    private lateinit var currentPhotoPath: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_choose_bottom_sheet, container, false)
+        _binding = FragmentImageChooseBottomSheetBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding?.apply {
+            btnCamera.setOnClickListener {
+                startTakePhoto()
+            }
+            btnGallery.setOnClickListener {
+                startGallery()
+            }
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == AppCompatActivity.RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+
+            val intent = Intent(requireContext(), SearchByImageActivity::class.java)
+            intent.putExtra("BitmapPath", myFile.path)
+            startActivity(intent)
+        }
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(requireActivity().packageManager)
+
+        createCustomTempFile(requireActivity().application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                requireContext(),
+                "com.c23ps291.heiwan",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, getString(R.string.choose_a_pic))
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, requireContext())
+
+            val intent = Intent(requireContext(), SearchByImageActivity::class.java)
+            intent.putExtra("BitmapPath", myFile.path)
+            startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    activity,
+                    R.string.camera_failed_msg,
+                    Toast.LENGTH_SHORT
+                ).show()
+                this@ImageChooseBottomSheetFragment.isRemoving
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
     companion object {
         const val TAG = "ModalBottomSheet"
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
+
 
 }
--- app/src/main/java/com/c23ps291/heiwan/ui/search/SearchByImageActivity.kt
+++ app/src/main/java/com/c23ps291/heiwan/ui/search/SearchByImageActivity.kt
@@ -1,24 +1,35 @@
 package com.c23ps291.heiwan.ui.search
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.c23ps291.heiwan.R
 import com.c23ps291.heiwan.databinding.ActivitySearchByImageBinding
 
+
 class SearchByImageActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchByImageBinding
+    private var searchImg: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchByImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+        searchImg = intent.getStringExtra("BitmapPath")
         binding.appBarSecondary.toolbar.apply {
             title = resources.getString(R.string.result)
             setNavigationOnClickListener { finish() }
         }
 
-        binding.btnSearch.setOnClickListener { }
+        binding.apply {
+            if (searchImg != null) {
+                val result = BitmapFactory.decodeFile(searchImg)
+                ivAnimal.setImageBitmap(result)
+            }
+
+        }
     }
 }
